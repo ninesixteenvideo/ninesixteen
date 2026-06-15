9917b633d9d4c3bbd52ef3a06413eba4f54338a1
ninesixteenvideo <ninesixteen.video@gmail.com>
2026-06-15 12:29:34 +0800

    firebase

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -6,14 +6,36 @@ const nextConfig = {
     optimizePackageImports: ["firebase"],
   },
   async headers() {
+    const authDomain =
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ??
+      "ninesixteenvideo-732af.firebaseapp.com";
+
     const csp = [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://apis.google.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://apis.google.com https://www.gstatic.com",
       "style-src 'self' 'unsafe-inline'",
-      "img-src 'self' data: blob: https:",
+      "img-src 'self' data: blob: https: https://lh3.googleusercontent.com",
       "font-src 'self' data:",
-      "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://api.stripe.com wss://*.firebaseio.com",
-      "frame-src https://js.stripe.com https://hooks.stripe.com",
+      [
+        "connect-src 'self'",
+        "https://*.googleapis.com",
+        "https://*.google.com",
+        "https://*.firebaseio.com",
+        "https://identitytoolkit.googleapis.com",
+        "https://securetoken.googleapis.com",
+        "https://firebase.googleapis.com",
+        "https://www.googleapis.com",
+        "https://api.stripe.com",
+        "wss://*.firebaseio.com",
+      ].join(" "),
+      [
+        "frame-src 'self'",
+        `https://${authDomain}`,
+        "https://accounts.google.com",
+        "https://www.google.com",
+        "https://js.stripe.com",
+        "https://hooks.stripe.com",
+      ].join(" "),
       "object-src 'none'",
       "base-uri 'self'",
       "form-action 'self'",
